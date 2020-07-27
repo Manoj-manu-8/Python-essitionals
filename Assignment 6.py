ae8c5c12774a9c8545946c6849c89e649ad986da
Manoj-manu-8 <68598363+Manoj-manu-8@users.noreply.github.com>
2020-07-27 22:48:29 +0530

    Add files via upload

---
+++ Assignment 6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+1) Write a program such that given two list covert it to a dictionary without using zip method?
+# In[7]:
+
+
+l1=[1,2,3,4,5]
+l2=[10,20,30,40,50]
+dict={l1[i]:l2[i] for i in range (len(l1))}
+print(dict)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
